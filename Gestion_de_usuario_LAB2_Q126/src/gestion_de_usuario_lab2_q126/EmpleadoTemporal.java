783040c41eaf96457c353191adb22c82ff5ec21d
Mario Moran <mjosuemoran029@gmail.com>
2026-01-30 09:34:03 -0600

    Avance de la clase EmpleadoTemporal

--- Gestion_de_usuario_LAB2_Q126/src/gestion_de_usuario_lab2_q126/EmpleadoTemporal.java
+++ Gestion_de_usuario_LAB2_Q126/src/gestion_de_usuario_lab2_q126/EmpleadoTemporal.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gestion_de_usuario_lab2_q126;
 
-/**
- *
- * @author mjosu
- */
-public class EmpleadoTemporal {
-    
+import java.util.Calendar;
+
+public class EmpleadoTemporal extends Empleado {
+    private Calendar fechaFinContrato;
+
+    // el construtor
+    public EmpleadoTemporal(String codigo, String nombre, double salarioBase, String rutaFoto, Calendar fechaFinContrato) {
+        super(codigo, nombre, salarioBase, rutaFoto);
+        this.fechaFinContrato = fechaFinContrato;
+    }
+
+    // fecha fin de contrato
+    public void actualizarFechaFinContrato(Calendar nuevaFecha) {
+        this.fechaFinContrato = nuevaFecha;
+    }
+
+    // pago condicionado
+    @Override
+    public double calcularPago() {
+        Calendar hoy = Calendar.getInstance();
+        if (hoy.compareTo(fechaFinContrato)<=0) {
+            return super.calcularPago();
+        } else {
+            return 0.0;
+        }
+    }
+
+    // informacion extendida
+    @Override
+    public String mostrarInformacion() {
+        return super.mostrarInformacion() +
+               " | Fin de contrato: " +
+               fechaFinContrato.get(Calendar.DAY_OF_MONTH) + "/" +
+               (fechaFinContrato.get(Calendar.MONTH) + 1) + "/" +
+               fechaFinContrato.get(Calendar.YEAR);
+    }
 }
