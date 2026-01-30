baaef2d33d88bbe0334ed2c55e8c6f0cbddeae7c
Joshua Mendez <joshuagmendez14@gmail.com>
2026-01-30 09:33:06 -0600

    Clase EmpleadoVentas updated

--- Gestion_de_usuario_LAB2_Q126/src/gestion_de_usuario_lab2_q126/EmpleadoVentas.java
+++ Gestion_de_usuario_LAB2_Q126/src/gestion_de_usuario_lab2_q126/EmpleadoVentas.java
@@ -3,11 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package gestion_de_usuario_lab2_q126;
-
+import java.util.Calendar;
 /**
  *
  * @author sandr
  */
-public class EmpleadoVentas {
+
+public class EmpleadoVentas extends Empleado{
+    double[] ventasMensual;
+    double tasaComision;
+    public EmpleadoVentas(String code, String nombre, double salarioBase, String ruteFoto, double tasaComision ){
+    super(code, nombre, salarioBase, ruteFoto);
+    this.tasaComision=tasaComision;
+    this.ventasMensual=new double[12];
+}
+    
+    public void registrarVenta(double monto){
+        Calendar diaHoy=Calendar.getInstance();
+        int mes;
+        mes=diaHoy.get(Calendar.MONTH);
+        //suma el monto dependiendo del mess. import calander monnth empieza desde 0, compatible con arreglos
+        this.ventasMensual[mes]= this.ventasMensual[mes]+monto;
+       
+        
+    }
     
 }
