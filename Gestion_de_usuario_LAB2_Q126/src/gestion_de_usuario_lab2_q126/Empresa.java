3c26ba45043dd4826ae6108ca563c9abd63d5043
Nathan Alegria <nathanalegria02@gmail.com>
2026-01-30 09:38:39 -0600

    Clase Empresa

---
+++ Gestion_de_usuario_LAB2_Q126/src/gestion_de_usuario_lab2_q126/Empresa.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestion_de_usuario_lab2_q126;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Nathan
+ */
+
+//Gestión general de empleados
+public class Empresa {
+
+    private Empleado[] empleados;
+    private int contador;
+
+    public Empresa(int capacidad) {
+        empleados = new Empleado[capacidad];
+        contador = 0;
+    }
+
+    public boolean registrarEmpleado(Empleado e) {
+        if (buscarEmpleado(e.getCodigo()) != null || contador >= empleados.length) {
+            return false;
+        }
+        empleados[contador++] = e;
+        return true;
+    }
+
+    public Empleado buscarEmpleado(String codigo) {
+        for (int i = 0; i < contador; i++) {
+            if (empleados[i].getCodigo().equals(codigo)) {
+                return empleados[i];
+            }
+        }
+        return null;
+    }
+
+    public void registrarHoras(String codigo, int horas) {
+        Empleado e = buscarEmpleado(codigo);
+        if (e != null) {
+            e.registrarHoras(horas);
+        }
+    }
+
+    public void registrarVenta(String codigo, double monto) {
+        Empleado e = buscarEmpleado(codigo);
+        if (e instanceof EmpleadoVentas) {
+            ((EmpleadoVentas) e).registrarVenta(monto);
+        }
+    }
+
+    public void actualizarContrato(String codigo, Calendar fecha) {
+        Empleado e = buscarEmpleado(codigo);
+        if (e instanceof EmpleadoTemporal) {
+            ((EmpleadoTemporal) e).actualizarFechaFinContrato(fecha);
+        }
+    }
+
+    public double calcularPagoEmpleado(String codigo) {
+        Empleado e = buscarEmpleado(codigo);
+        return (e != null) ? e.calcularPago() : 0;
+    }
+
+    public String generarReporte() {
+        String reporte = "";
+        int est = 0, temp = 0, ven = 0;
+
+        for (int i = 0; i < contador; i++) {
+            reporte += empleados[i].mostrarInformacion() + "\n";
+            if (empleados[i] instanceof EmpleadoVentas) ven++;
+            else if (empleados[i] instanceof EmpleadoTemporal) temp++;
+            else est++;
+        }
+
+        reporte += "\nEstándar: " + est +
+                   "\nTemporales: " + temp +
+                   "\nVentas: " + ven;
+
+        return reporte;
+    }
+}
+
