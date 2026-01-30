f04b1f9daf795859904ee5605e9330bb81440b39
Andrés Díaz <andresalbertodiazrevelo@gmail.com>
2026-01-30 08:22:59 -0600

    paquete de trabajo

---
+++ Gestion_de_usuario_LAB2_Q126/src/gestion_de_usuario_lab2_q126/Gestion_de_usuario_LAB2_Q126.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestion_de_usuario_lab2_q126;
+
+/**
+ *
+ * @author user
+ */
+public class Gestion_de_usuario_LAB2_Q126 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
